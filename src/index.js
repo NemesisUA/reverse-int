d3e5450dd80f8c6a19486804a798e8e07ed50c25
NemesisUA <display.n0ne.01@gmail.com>
2020-10-12 11:15:22 +0300

    feat procedure reverse integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let arrayRevers = [];
+
+  let intToReverse = Math.abs(n);
+  let strToReverse = intToReverse.toString();
+  arrayReverse = strToReverse.split('').reverse().join('');  
+    
+ return  arrayReverse;
 }
